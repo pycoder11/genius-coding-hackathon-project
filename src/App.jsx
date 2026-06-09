7b10356cf273cb0d031bf5e847aa7e942fcd73f3
pycoder11 <zetshia@gmail.com>
2026-06-09 16:51:13 -0400

    Fix template string syntax for iconUrl

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ const getMarkerIcon = (type) => {
   if (type === 'Medical') color = 'violet';
 
   return new L.Icon({
-    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-${color}.png',
+    iconUrl: `https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-${color}.png`,
     shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
     iconSize: [25, 41],
     iconAnchor: [12, 41],
